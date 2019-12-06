9dd5d4faa37af7cd20ab6938db3f00a082610f48
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-06 13:49:17 +0700

    Add main()

--- src/main.py
+++ src/main.py
@@ -1,2 +1,20 @@
 import random as rand
 import tkinter as tk
+
+def main():
+    windowWidth = 800
+    windowHeight = 240
+    root = tk.Tk()
+    root.title = 'Star sky'
+    canvas = tk.Canvas(root, width = windowWidth, height = windowHeight)
+    canvas.pack()
+    continueDraw = True
+    while(continueDraw):
+        red = rand.randint(0, 255)
+        green = rand.randint(0, 255)
+        blue = rand.randint(0, 255)
+        color = (red, green, blue)
+        x = rand.randint(0, windowWidth)
+        y = rand.randint(0, windowHeight)
+        canvas.create_oval(x, y, x+1, +1, outline=color, fill=color)
+        root.update()
