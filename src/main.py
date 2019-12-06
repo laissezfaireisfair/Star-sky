bd0db628f3ce75dd0f63b171dbee931266ac0e22
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-06 14:15:25 +0700

    Fix color conversion

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ def main():
         red = rand.randint(0, 255)
         green = rand.randint(0, 255)
         blue = rand.randint(0, 255)
-        color = (red, green, blue)
+        color = '#' + '{0:x}{0:x}{0:x}'.format(red, green, blue)
         x = rand.randint(0, windowWidth)
         y = rand.randint(0, windowHeight)
         canvas.create_oval(x, y, x+1, +1, outline=color, fill=color)
