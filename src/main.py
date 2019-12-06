b7d2f7d5cfcb6d5e0a5fa2316b48981a7dc4ecc4
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-06 16:39:55 +0700

    Fix color range and oval draw

--- src/main.py
+++ src/main.py
@@ -10,11 +10,11 @@ def main():
     canvas.pack()
     continueDraw = True
     while(continueDraw):
-        red = rand.randint(0, 255)
-        green = rand.randint(0, 255)
-        blue = rand.randint(0, 255)
-        color = '#' + '{0:x}{0:x}{0:x}'.format(red, green, blue)
+        red = rand.randint(0, 4095)
+        green = rand.randint(0, 4095)
+        blue = rand.randint(0, 4095)
+        color = '#' + '{0:03x}{1:03x}{2:03x}'.format(red, green, blue)
         x = rand.randint(0, windowWidth)
         y = rand.randint(0, windowHeight)
-        canvas.create_oval(x, y, x+1, +1, outline=color, fill=color)
+        canvas.create_oval(x, y, x+1, y+1, outline=color, fill=color)
         root.update()
