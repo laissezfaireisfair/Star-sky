2dc9e7c6b488f74ee38565ebce9de68763621e8e
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-07 15:49:09 +0700

    Add exit after pressing key

--- src/main.py
+++ src/main.py
@@ -1,15 +1,26 @@
 import random as rand
 import tkinter as tk
 
+class Flag:
+    __value = False
+    def set_true(self, event=""):
+        self.__value = True
+    def set_false(self, event=""):
+        self.__value = False
+    def get(self):
+        return self.__value
+
 def main():
     windowWidth = 800
     windowHeight = 240
     root = tk.Tk()
     root.title = 'Star sky'
+    flag = Flag()
+    flag.set_true()
+    root.bind('<Key>', flag.set_false)
     canvas = tk.Canvas(root, width = windowWidth, height = windowHeight)
     canvas.pack()
-    continueDraw = True
-    while(continueDraw):
+    while(flag.get()):
         red = rand.randint(0, 4095)
         green = rand.randint(0, 4095)
         blue = rand.randint(0, 4095)
@@ -18,3 +29,4 @@ def main():
         y = rand.randint(0, windowHeight)
         canvas.create_oval(x, y, x+1, y+1, outline=color, fill=color)
         root.update()
+    root.destroy()
